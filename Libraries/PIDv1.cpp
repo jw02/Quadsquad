621754265d45acca9acad6648c0946e383beccc8
jw02 <jaredwilson002@gmail.com>
2016-08-24 07:14:01 -0500

    changed alot of variables to arrays

--- Libraries/PIDv1.cpp
+++ Libraries/PIDv1.cpp
@@ -2,28 +2,39 @@
 #include <PIDv1.h>
 
 //  variable declarations
-unsigned long tik;
-double Input, Output, setPoint;
-double lastPV;
-double kp, ki, kd, P, I, D;
 int sampleTime = 1000;
-double outMin, outMax;
+int nVars = 0;
 bool inAuto = false;
+
+//	declare arrays
+double Input[NUMBER_OF_VARIABLES];
+double Output[NUMBER_OF_VARIABLES];
+double lastPV[NUMBER_OF_VARIABLES];
+double I[NUMBER_OF_VARIABLES];
+unsigned long tik[NUMBER_OF_VARIABLES];
+
+int f = NUMBER_OF_VARIABLES*3;
+double tunePs[f];
+int g = NUMBER_OF_VARIABLES*2;
+double outMin[g];
+double outMax[g];
+g = 0;
+f = g;
 
 int controllerDirection = DIRECT;
 
-void setControllerDirection (int Direction){
+void setControllerDirection (int Direction) {
   controllerDirection = Direction;
 }
 
-void Initialize() {
-  lastPV = Input;
-  I = Output;
-  if(I > outMax) I = outMax;
-  else if(I < outMin) I = outMin;  
+void setOutputLimits(double Min, double Max) {
+  if(Min > Max) return;
+  outMin[(nVars*2)] = Min;
+  outMax[(nVars*2) + 1] = Max;
+  nVars++;
 }
 
-void setMode(int Mode){
+void setMode(int Mode) {
   bool newAuto = (Mode == AUTOMATIC);
   if(newAuto && !inAuto) {
     Initialize();
@@ -31,73 +42,89 @@ void setMode(int Mode){
   inAuto = newAuto;
 }
 
-void setOutputLimits(double Min, double Max) {
-  if(Min > Max) return;
-  outMin = Min;
-  outMax = Max;
-}
-
-void setNewSampleTime(int NewSampleTime)
-{
+int c1 = 0;
+void setNewSampleTime(int NewSampleTime) {
    if (NewSampleTime > 0)
    {
       double ratio  = (double)NewSampleTime
                       / (double)sampleTime;
-      ki *= ratio;
-      kd /= ratio;
+      tunePs[nVars*c1 + 1] *= ratio;
+      tunePs[nVars*c1 + 2] /= ratio;
       sampleTime = (unsigned long)NewSampleTime;
    }
 }
 
+int c2 = 0;
 void setTunings(double Kp, double Ki, double Kd){
 
   if(Kp < 0 || Ki < 0 || Kd < 0) return;
   
   double SampleTimeInSec = double(sampleTime/1000.0);
   
-  kp = Kp;
-  ki = Ki * SampleTimeInSec;
-  kd = Kd / SampleTimeInSec;
+  tunePs[nVars*c2] = Kp;
+  tunePs[nVars*c2 + 1] = Ki * SampleTimeInSec;
+  tunePs[nVars*c2 + 2] = Kd / SampleTimeInSec;
 
   if(controllerDirection == REVERSE){
-    kp = (0 - kp);
-    ki = (0 - ki);
-    kd = (0 - kd);
+    tunePs[nVars*c2] = (0 - tunePs[nVars*c2]);
+    tunePs[nVars*c2 + 1] = (0 - tunePs[nVars*c2 + 2]);
+    tunePs[nVars*c2 + 2] = (0 - tunePs[nVars*c2 + 2]);
   }
+  c2++;
 }
 
-int compute(int sP, int pV, double a, double b, double c){
-  
-  if(!inAuto) return 999;
+//	compute has to be called in the same order as the tuning parameters were set in the main code
+//	ie tuning parameters were set as: 0 - Pitch, 1 - Roll, 2 - Yaw, then call compute in that same order
+//
+int c3 = 0;
+int compute(int sP, int pV){
+ 
+	if(!inAuto) return 999;
+	unsigned long tok = millis();
+	int tiktok = tok - tik;
+	
+	if(tiktok >= sampleTime){
+		double kp, ki, kd, P, D;
+		
+		kp = tunePs[nVars*c3];
+		ki = tunePs[nVars*c3 + 1];
+		kd = tunePs[nVars*c3 + 2];
+		
+		double error = sP - pV;
+		I[c3] += ki*error;
+		double dPV = (pV - lastPV[c3]);
+		
+		P = kp*error;
+		D = -kd*dPV;
+		
+		Output[c3] = P + I + D;
+		if(Output[c3] > outMax[c3*2 + 1]){
+			I -= Output[c3] - outMax[c3*2 + 1];
+			Output[c3] = outMax[c3*2 + 1];
+		}
+		else if(Output[c3] < outMin[c3*2]){
+			I += outMin[c3*2] - Output[c3];
+			Output[c3] = outMin[c3*2];
+		}
+		
+		Input[c3] += Output[c3];
+		lastPV[c3] = Input[c3];
+		tik = tok;
+		c3++;
+		if(c3 > nVars) c3 = 0;
+		
+		Input[c3] += 0.5;
+		int result = (int) Input[c3];
+		return result;
+	}
+}
 
-  setTunings(a,b,c);
-  unsigned long tok = millis();
-  int tiktok = tok - tik;
-  
-  if(tiktok >= sampleTime){
-    double error = sP - pV;
-    I += ki*error;
-    double dPV = (pV - lastPV);
-        
-    P = kp*error;
-    D = -kd*dPV;
-
-    Output = P + I + D;
-    if(Output > outMax){
-      I -= Output - outMax;
-      Output = outMax;
-    }
-    else if(Output < outMin){
-      I += outMin - Output;
-      Output = outMin;
-    }
-    
-    Input += Output;
-    lastPV = Input;
-    tik = tok;
-	Input += 0.5;
-	int result = (int) Input;
-
-	return result;
-  }
+int c4 = 0;
+void Initialize() {
+	for(int i = 0; i < nVars; i++) {
+		lastPV[i] = Input[i];
+		I[i] = Output[i];
+		if(I > outMax[i*2 + 1]) I = outMax[i*2 + 1];
+		else if(I < outMin[i*2]) I = outMin[i*2];  
+	}
 }
